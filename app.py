04bbd629a5134f67359c3e1f8573e2c566df5c9b
Vaibhav Solanke <vaibhavsolanke29@gmail.com>
2026-06-30 21:55:38 +0530

    Refactor image recommendation system in app.py
    Refactor image processing and recommendation logic; update file paths and variable names.

--- app.py
+++ app.py
@@ -21,8 +21,17 @@ import torchvision
 from torchvision import transforms, models
 from PIL import Image
 import uuid
-from natsort import natsorted
 import cv2 as cv
+from natsort import natsorted
+
+df_master = pd.read_csv('Image_data.csv')
+image_text_list = df_master['Image_Description'].str.lower().tolist()
+images_name = df_master['Image_Name'].tolist()
+imagedir = 'uploaded_images/'
+
+img_f = torch.load("image_features1",
+                     map_location="cpu",
+                     weights_only=False)
 
 app = FastAPI()
 
@@ -33,60 +42,38 @@ async def disable_cache(request: Request, call_next):
     response.headers["Pragma"] = "no-cache"
     return response
 
-
-imagedir = 'images/'
-images__ = 'Images_1/'
-
-images_list = natsorted(os.listdir(images__))
-
-df = pd.read_excel('dataset.xlsx')
-
-image_text_list=df['image_name'].str.lower().str.replace('-',' ').tolist()
-
-model = models.densenet121(weights='DEFAULT')
-model.eval()
-
-feature_extractor=model.features
-
-image_features=[]
-
-for i in images_list:
-
-
-  preprocess = transforms.Compose([transforms.Resize(256),
-                                 transforms.CenterCrop(224),
-                                 transforms.ToTensor(),
-                                 transforms.Normalize(mean=[0.428,0.456,0.406],
-                                                      std = [0.229,0.224,0.225])
-                                ])
-
-  input_tensor = preprocess(Image.open(f'Images_1/{i}').convert('RGB')).unsqueeze(0)
-
-  features = feature_extractor(input_tensor)
-  features = torch.nn.functional.adaptive_avg_pool2d(features,(1,1))
-  features = features.view(features.size(0),-1)
-
-  image_features.append(features.detach().numpy())
-
-image_features=np.vstack(image_features)
-
 @app.get('/image type')
-async def text_recommondation(text:str,number_of_images:int=Form(...)):
+async def text_recommondation(Image_Description:str, Number_Of_Images:int):
 
   vec = TfidfVectorizer(use_idf=False,
                         norm=None)
 
-  text=text.lower()
+  text=Image_Description.lower()
   combined_text = image_text_list+[text]
   result = vec.fit_transform(combined_text)
 
   data_tfidf = pd.DataFrame(result.toarray(),columns=sorted(vec.vocabulary_))
 
-  idx=data_tfidf[[i for i in text.split() if i in vec.vocabulary_.keys()]].sum(axis=1).sort_values(ascending=False).index.to_list()[:number_of_images]
+  idx=data_tfidf[[i for i in text.split() if i in vec.vocabulary_.keys()]].sum(axis=1).sort_values(ascending=False).index.to_list()[:Number_Of_Images]
 
+  img = []
+  for k in idx:
+      a = Image.open(f'jpg/{images_name[k]}')
+      a = a.resize([300, 300])
+      a = np.array(a)
+      a = cv.cvtColor(a, cv.COLOR_BGR2RGB)
+      img.append(a)
+  combined_img = cv.hconcat(img)
+  img__, combined_img = cv.imencode('.jpg', combined_img)
 
-  image_path = f'{images__}{images_list[idx[0]]}'
-  return FileResponse(f'Images_1/{images_list[idx[0]]}')
+  return Response(content=combined_img.tobytes(), media_type='image/jpeg')
+
+model_densenet121 = models.densenet121(weights='DEFAULT')
+model_densenet121.eval()
+feature_extractor = model_densenet121.features
+img_feat = np.vstack(img_f)
+print(img_feat)
+print(df_master[['Image_Name','Image_Features']])
 
 @app.post("/add_image")
 async def img_reccomondation(upload_image: UploadFile = File(...),Number_of_images:int = Form(...)):
@@ -95,7 +82,7 @@ async def img_reccomondation(upload_image: UploadFile = File(...),Number_of_imag
 
     with open(f'{imagedir}{upload_image.filename}', 'wb') as f:
         f.write(contents)
-    image_path = Path('images/' + upload_image.filename)
+    image_path = Path('uploaded_images/' + upload_image.filename)
     im_=FileResponse(image_path)
 
     preprocess = transforms.Compose([transforms.Resize(256),
@@ -112,7 +99,7 @@ async def img_reccomondation(upload_image: UploadFile = File(...),Number_of_imag
     features = features.view(features.size(0),-1)
     features = features.detach().numpy()
 
-    similarity = cosine_similarity(features,image_features)
+    similarity = cosine_similarity(features,img_feat)
 
     similarity = similarity.flatten()
 
@@ -120,7 +107,7 @@ async def img_reccomondation(upload_image: UploadFile = File(...),Number_of_imag
 
     img=[]
     for k in idx_:
-        a=Image.open(f'Images_1/{images_list[k]}')
+        a=Image.open(f'jpg/{images_name[k]}')
         a=a.resize([300,300])
         a=np.array(a)
         a=cv.cvtColor(a,cv.COLOR_BGR2RGB)
@@ -128,7 +115,10 @@ async def img_reccomondation(upload_image: UploadFile = File(...),Number_of_imag
     combined_img = cv.hconcat(img)
     img__, combined_img = cv.imencode('.jpg', combined_img)
 
-    return Response(content=combined_img.tobytes(),media_type='image/jpeg')
+    if os.path.exists(image_path):
+        os.remove(image_path)
+
+    return Response(content=combined_img.tobytes(), media_type='image/jpeg')
 
 if __name__ == "__main__":
     uvicorn.run(app)
